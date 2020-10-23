74a240e1f80351f4ad072366103a967b2c5b85d7
MoReza <mz.abdollahi223@gmail.com>
2020-10-23 13:30:04 +0330

    A simple UI is added

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,50 @@
 const socket = io()
 const input = document.querySelector('#showPrice')
-const h1 = document.getElementsByTagName('h1')
+const clockElement = document.querySelector("#clock")
+const inputToShow = document.querySelector('#priceInput')
+const inputToRial = document.querySelector('#rial')
+const inputToBTC = document.querySelector('#btc')
+
+let priceToCalculate = 0
+
+inputToRial.addEventListener('change', (e) => {
+    e.preventDefault()
+    inputToBTC.value = inputToRial.value / (priceToCalculate * 1 * 297500)
+})
+
+inputToBTC.addEventListener('change', (e) => {
+    e.preventDefault()
+    inputToRial.value = inputToBTC.value * priceToCalculate * 1 * 297500
+})
 
 socket.on('price', price => {
     outPutPrice(price)
 })
 
+const updateClock = (clock) => {
+    clock.innerHTML = new Date().toLocaleTimeString()
+}
 
+setInterval(function () {
+    updateClock(clockElement);
+}, 1000)
 
 // Fetch
 const setPrice = () => {
-    console.log('hi')
     fetch('https://api.binance.com/api/v1/ticker/price?symbol=BTCUSDT')
         .then(response => response.json())
-        .then(data => socket.emit('Change', data))
+        .then(data => {
+            socket.emit('Change', data)
+            priceToCalculate = data.price
+        })
 }
 
 const outPutPrice = (price) => {
     console.log(price)
-    h1[0].innerHTML = price
+    inputToShow.value = `${price * 1 * 297500} ریال`
 }
 
-// Checking the Price every moment
+// Checking the Price every 10 Seconds
 setInterval(setPrice, 10000)
 // Reloading the page each 5 seconds
 /*setTimeout(() => {
